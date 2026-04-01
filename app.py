e8f88ca68a44f60d2a02dc9d37f38654f51f1a9f
Laurenz Brödemann <laurenzbrodemann@MacBookPro.fritz.box>
2026-04-01 17:54:48 +0200

    Zweite Seite und Navigation hinzugefügt

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/tippspiel")
+def tippspiel():
+    return render_template("tippspiel.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
