840997d43dbc24fdfa78c4b23d1b01a16c2e2ed6
Laurenz Brödemann <laurenzbrodemann@MacBookPro.fritz.box>
2026-04-01 18:22:02 +0200

    Login- und Registrierungsseiten hinzugefügt

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("login.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 @app.route("/tippspiel")
 def tippspiel():
